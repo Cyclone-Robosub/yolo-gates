37e5e97458b59b39fc0479ac50e50e4e112eedeb
Dannykcw <danny.kwong06@gmail.com>
2025-02-12 14:33:10 -0800

    fixed train.py

--- train.py
+++ train.py
@@ -5,18 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-rf = Roboflow(api_key=os.getenv("ROBOFLOW", default=None))
+rf = Roboflow(api_key=os.getenv("ROBOFLOW"))
 project = rf.workspace("robosub").project("pre-qualification-gate")
 version = project.version(4)
-dataset = version.download("yolov8")
+dataset = version.download("yolov8",location="datasets")
 
 # Load a pre-trained YOLOv10n model
 model = YOLO("yolov10n.pt")
 
-model.train(data="Pre-qualification-Gate-4/data.yaml", epochs=50, imgsz=640, batch=16, device='mps')
+
+model.train(data="datasets/Pre-qualification-Gate-4/data.yaml", epochs=50, imgsz=640, batch=16, device='mps')
 
 # Perform object detection on an image
-results = model("Pre-qualification-Gate-4/test/images/frame110_jpg.rf.abc86f03d152466fb80f5471826d260e.jpg")
+results = model("datasets/Pre-qualification-Gate-4/test/images/frame110_jpg.rf.abc86f03d152466fb80f5471826d260e.jpg")
 
 # Display the results
 results[0].show()
